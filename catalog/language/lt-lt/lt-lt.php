8ff95f5283037484e482ad0b06315eb24065b7b0
mantaspau98 <30447457+mantaspau98@users.noreply.github.com>
2019-05-22 18:25:37 +0100

    Update lt-lt.php

--- catalog/language/lt-lt/lt-lt.php
+++ catalog/language/lt-lt/lt-lt.php
@@ -122,259 +122,26 @@ $_['button_map']            = 'Google žemėlapis';
 
 $_['error_exception']       = 'Klaidos kodas(%s): %s yra %s eilutėje %s';
 
-$_['error_upload_1']        = 'Įspėjimas: įkeltas failas viršija upload_max_filesize direktyvą php.ini!';
+$_['error_upload_1']        = 'Įspėjimas: įkeltas failas viršija upload_max_filesize direktyvą php.ini';
 
-$_['error_upload_2']        = 'Įspėjimas: įkeltas failas viršija MAX_FILE_SIZE direktyvą, nurodytą HTML formoje!';
+$_['error_upload_2']        = 'Įspėjimas: įkeltas failas viršija MAX_FILE_SIZE direktyvą, nurodytą HTML formoje';
 
-$_['error_upload_3']        = 'Įspėjimas: įkeltas failas buvo iš dalies įkeltas tik iš dalies!';
+$_['error_upload_3']        = 'Įspėjimas: įkeltas failas buvo iš dalies įkeltas tik iš dalies';
 
-$_['error_upload_4']        = 'Įspėjimas: failas nebuvo ikeltas!';
+$_['error_upload_4']        = 'Įspėjimas: failas nebuvo ikeltas';
 
-$_['error_upload_6']        = 'Įspėjimas: nėra laikino aplanko!';
+$_['error_upload_6']        = 'Įspėjimas: nėra laikino aplanko';
 
-$_['error_upload_7']        = 'Įspėjimas: Nepavyko įrašyti į diską!';
+$_['error_upload_7']        = 'Įspėjimas: Nepavyko įrašyti į diską';
 
-$_['error_upload_8']        = 'Įspėjimas: Failo įkėlimas buvo sustabdytas papildinio!';
+$_['error_upload_8']        = 'Įspėjimas: Failo įkėlimas buvo sustabdytas';
 
-$_['error_upload_999']      = 'Įspėjimas: Nėra klaidos kodo!';
+$_['error_upload_999']      = 'Įspėjimas: Nėra klaidos kodo';
 
 $_['error_curl']            = 'CURL: Klaidos kodas(%s): %s';
 
 
 
-/* When doing translations only include the matching language code */
 
-
-
-// Datepicker
-
-//$_['datepicker']            = 'af';
-
-//$_['datepicker']            = 'ar-dz';
-
-//$_['datepicker']            = 'ar-kw';
-
-//$_['datepicker']            = 'ar-ly';
-
-//$_['datepicker']            = 'ar-ma';
-
-//$_['datepicker']            = 'ar-sa';
-
-//$_['datepicker']            = 'ar-tn';
-
-//$_['datepicker']            = 'ar';
-
-//$_['datepicker']            = 'az';
-
-//$_['datepicker']            = 'be';
-
-//$_['datepicker']            = 'bg';
-
-//$_['datepicker']            = 'bn';
-
-//$_['datepicker']            = 'bo';
-
-//$_['datepicker']            = 'br';
-
-//$_['datepicker']            = 'bs';
-
-//$_['datepicker']            = 'ca';
-
-//$_['datepicker']            = 'cs';
-
-//$_['datepicker']            = 'cv';
-
-//$_['datepicker']            = 'cy';
-
-//$_['datepicker']            = 'da';
-
-//$_['datepicker']            = 'de-at';
-
-//$_['datepicker']            = 'de-ch';
-
-//$_['datepicker']            = 'de';
-
-//$_['datepicker']            = 'dv';
-
-//$_['datepicker']            = 'el';
-
-//$_['datepicker']            = 'en-au';
-
-//$_['datepicker']            = 'en-ca';
-
-//$_['datepicker']            = 'en-gb';
-
-//$_['datepicker']            = 'en-ie';
-
-//$_['datepicker']            = 'en-nz';
-
-//$_['datepicker']            = 'eo';
-
-//$_['datepicker']            = 'es-do';
-
-//$_['datepicker']            = 'es';
-
-//$_['datepicker']            = 'et';
-
-//$_['datepicker']            = 'eu';
-
-//$_['datepicker']            = 'fa';
-
-//$_['datepicker']            = 'fi';
-
-//$_['datepicker']            = 'fo';
-
-//$_['datepicker']            = 'fr-ca';
-
-//$_['datepicker']            = 'fr-ch';
-
-//$_['datepicker']            = 'fr';
-
-//$_['datepicker']            = 'fy';
-
-//$_['datepicker']            = 'gd';
-
-//$_['datepicker']            = 'gl';
-
-//$_['datepicker']            = 'gom-latn';
-
-//$_['datepicker']            = 'he';
-
-//$_['datepicker']            = 'hi';
-
-//$_['datepicker']            = 'hr';
-
-//$_['datepicker']            = 'hu';
-
-//$_['datepicker']            = 'hy-am';
-
-//$_['datepicker']            = 'id';
-
-//$_['datepicker']            = 'is';
-
-//$_['datepicker']            = 'it';
-
-//$_['datepicker']            = 'ja';
-
-//$_['datepicker']            = 'jv';
-
-//$_['datepicker']            = 'ka';
-
-//$_['datepicker']            = 'kk';
-
-//$_['datepicker']            = 'km';
-
-//$_['datepicker']            = 'kn';
-
-//$_['datepicker']            = 'ko';
-
-//$_['datepicker']            = 'ky';
-
-//$_['datepicker']            = 'lb';
-
-//$_['datepicker']            = 'lo';
-
-$_['datepicker']            = 'lt-lt';
-
-//$_['datepicker']            = 'lv';
-
-//$_['datepicker']            = 'me';
-
-//$_['datepicker']            = 'mi';
-
-//$_['datepicker']            = 'mk';
-
-//$_['datepicker']            = 'ml';
-
-//$_['datepicker']            = 'mr';
-
-//$_['datepicker']            = 'ms-my';
-
-//$_['datepicker']            = 'ms';
-
-//$_['datepicker']            = 'my';
-
-//$_['datepicker']            = 'nb';
-
-//$_['datepicker']            = 'ne';
-
-//$_['datepicker']            = 'nl-be';
-
-//$_['datepicker']            = 'nl';
-
-//$_['datepicker']            = 'nn';
-
-//$_['datepicker']            = 'pa-in';
-
-//$_['datepicker']            = 'pl';
-
-//$_['datepicker']            = 'pt-br';
-
-//$_['datepicker']            = 'pt';
-
-//$_['datepicker']            = 'ro';
-
-//$_['datepicker']            = 'ru';
-
-//$_['datepicker']            = 'sd';
-
-//$_['datepicker']            = 'se';
-
-//$_['datepicker']            = 'si';
-
-//$_['datepicker']            = 'sk';
-
-//$_['datepicker']            = 'sl';
-
-//$_['datepicker']            = 'sq';
-
-//$_['datepicker']            = 'sr-cyrl';
-
-//$_['datepicker']            = 'sr';
-
-//$_['datepicker']            = 'ss';
-
-//$_['datepicker']            = 'sv';
-
-//$_['datepicker']            = 'sw';
-
-//$_['datepicker']            = 'ta';
-
-//$_['datepicker']            = 'te';
-
-//$_['datepicker']            = 'tet';
-
-//$_['datepicker']            = 'th';
-
-//$_['datepicker']            = 'tl-ph';
-
-//$_['datepicker']            = 'tlh';
-
-//$_['datepicker']            = 'tr';
-
-//$_['datepicker']            = 'tzl';
-
-//$_['datepicker']            = 'tzm-latn';
-
-//$_['datepicker']            = 'tzm';
-
-//$_['datepicker']            = 'uk';
-
-//$_['datepicker']            = 'ur';
-
-//$_['datepicker']            = 'uz-latn';
-
-//$_['datepicker']            = 'uz';
-
-//$_['datepicker']            = 'vi';
-
-//$_['datepicker']            = 'x-pseudo';
-
-//$_['datepicker']            = 'yo';
-
-//$_['datepicker']            = 'zh-cn';
-
-//$_['datepicker']            = 'zh-hk';
-
-//$_['datepicker']            = 'zh-tw';
+$_['datepicker']            = 'lt-lt';    
 
