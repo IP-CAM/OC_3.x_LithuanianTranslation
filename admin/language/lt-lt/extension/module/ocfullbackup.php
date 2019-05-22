5a21587548586e1b7707f0df6be9a12dae92df80
MantasPau <mantas.pau@gmail.com>
2019-05-22 15:40:34 +0100

    Admin side initial commit

---
+++ admin/language/lt-lt/extension/module/ocfullbackup.php
@@ -0,0 +1,38 @@
+<?php
+// Heading
+$_['heading_title'] 		= 'OC full backup';
+// Text
+$_['text_extension']		= 'Extensions';
+$_['text_success']		    = 'Success: You have modified OC full backup module!';
+$_['text_edit']			    = 'Edit OC full backup Module';
+// Entry
+$_['entry_name']		= 'Module ID';
+$_['entry_message']		= 'Message';
+$_['entry_button_text']			= 'Button text';
+$_['entry_bg_colour'] 	= 'Background colour';
+$_['entry_text_colour'] 	= 'Text colour';
+$_['entry_status'] = 'Status';
+$_['entry_cookies_informations'] = 'Information link';
+$_['entry_informations_text'] = 'Information link text';
+$_['entry_show_type'] = 'How to show the popup';
+
+// Placeholder
+$_['placeholder_name'] 		= '';
+$_['placeholder_message'] 	= '';
+$_['placeholder_button_text'] 		= '';
+$_['placeholder_bg_colour'] 	= '#ffc520';
+$_['placeholder_text_colour'] 	= '#fff';
+$_['placeholder_informations_text']   = 'More Information';
+// Error
+$_['error_name'] 		= 'Module ID blank';
+$_['error_message'] 		= 'Message field blank';
+$_['error_button_text'] 		= 'Button text is blank';
+$_['error_bg_colour'] 	= 'Background colour can not be empty';
+$_['error_text_colour'] 	= 'Text colour can not be empty';
+$_['error_informations_text']         = 'Text colour can not be empty';
+$_['error_warning'] 		= 'There are problems with data provided';
+$_['error_permission'] 		= 'You don\'t have permission to edit this.';
+	
+// Button
+$_['button_save'] 		= 'Save form';
+$_['button_cancel'] 		= 'Cancel';
